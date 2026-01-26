6f6f1a3e58f4de489ac23e958557d28f33bc4da8
Javascript1414 <soumosantra588@gmail.com>
2026-01-26 21:33:58 +0530

    init: sou

---
+++ dowhile.js
@@ -0,0 +1,17 @@
+let n=0
+do {
+    console.log("Number=",n);
+    n++
+}while (n<3);
+
+let m=0
+do {
+    console.log("Number=",m);
+    m++
+}while (m<=3);
+
+let o=0
+do {
+    console.log("Number=",o);
+    o++
+}while (o>3);
